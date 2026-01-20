3fa9188b923bdd18963f32cd1e199070d3a3a791
digis <digis@192.167.0.68>
2026-01-20 11:53:43 -0600

    Inicializacion de proyecto de BancoService

---
+++ src/main/java/com/Banco/CajerosService/CajerosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Banco.CajerosService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajerosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajerosServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Banco/CajerosService/CajerosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Banco.CajerosService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajerosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
